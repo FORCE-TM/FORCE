3ccd2201ded12532aa9a39c12518aca0844c81ee
Laiteux <matt@laiteux.dev>
2022-03-17 18:21:04 +0100

    Add some very light controller base and usage example

---
+++ src/FORCE.Core/ForceController.cs
@@ -0,0 +1,13 @@
+﻿using GbxRemoteNet;
+
+namespace FORCE.Core;
+
+public class ForceController
+{
+    public GbxRemoteClient Client { get; }
+
+    public ForceController(string serverHost = "127.0.0.1", int serverPort = 5000)
+    {
+        Client = new GbxRemoteClient(serverHost, serverPort);
+    }
+}
--- src/FORCE/Program.cs
+++ src/FORCE/Program.cs
@@ -1,8 +1,55 @@
-﻿namespace FORCE;
+﻿using FORCE.Core;
+using GbxRemoteNet.Structs;
+
+namespace FORCE;
 
 internal static class Program
 {
     public static async Task Main()
     {
+        var force = new ForceController();
+
+        Console.WriteLine("Connecting...");
+
+        if (!await force.Client.ConnectAsync())
+        {
+            Console.WriteLine("Could not establish a connection to the server.");
+            return;
+        }
+
+        Console.WriteLine("Authenticating...");
+
+        try
+        {
+            await force.Client.AuthenticateAsync("SuperAdmin", "SuperAdmin");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Could not authenticate to the server: {ex.Message}");
+            return;
+        }
+
+        Console.WriteLine("Successfully connected and authenticated!" + Environment.NewLine);
+
+        PlayerInfo[] playerList = await force.Client.GetPlayerListAsync();
+        Console.WriteLine($"Online players: {playerList.Length}");
+
+        foreach (var player in playerList)
+        {
+            Console.WriteLine($"   - [{player.Login}] {player.NickName} (Spectator: {player.Spectator})");
+        }
+
+        await force.Client.EnableCallbacksAsync();
+        
+        Console.WriteLine(Environment.NewLine + "Chat:");
+
+        force.Client.OnPlayerChat += async (playerUid, login, text, isRegisteredCmd) =>
+        {
+            var player = await force.Client.GetPlayerInfoAsync(login);
+
+            Console.WriteLine($"   [{player.NickName}] {text}");
+        };
+
+        await Task.Delay(-1);
     }
 }
