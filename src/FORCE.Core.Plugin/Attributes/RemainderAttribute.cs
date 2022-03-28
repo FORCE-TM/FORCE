e535edfba748bf8d98223a4f3b50c5d694c18e1d
Laiteux <matt@laiteux.dev>
2022-03-28 18:59:52 +0200

    Add RemainderAttribute

---
+++ src/FORCE.Core.Plugin/Attributes/RemainderAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace FORCE.Core.Plugin;
+
+/// <summary>
+/// Indicates that starting from a parameter, the command arguments should not be split anymore.<br/>
+/// The parameter must be of type <see cref="string"/>, and there can not be more parameters after it.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter)]
+public class RemainderAttribute : Attribute, IRemainderAttribute
+{
+    /// <summary>
+    /// <inheritdoc cref="RemainderAttribute"/>
+    /// </summary>
+    public RemainderAttribute()
+    {
+        IsRemainder = true;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public bool IsRemainder { get; set; }
+}
+
+public interface IRemainderAttribute
+{
+    public bool IsRemainder { get; set; }
+}
