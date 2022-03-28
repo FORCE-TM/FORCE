20b0065acc93e45b1d0f3dbac91c20572357255c
Laiteux <matt@laiteux.dev>
2022-03-28 18:57:30 +0200

    Add SummaryAttribute

---
+++ src/FORCE.Core.Plugin/Attributes/SummaryAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace FORCE.Core.Plugin;
+
+/// <summary>
+/// Adds a summary to your plugin, command group, command or command parameter.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Parameter)]
+public class SummaryAttribute : Attribute, ISummaryAttribute
+{
+    /// <summary>
+    /// <inheritdoc cref="SummaryAttribute"/>
+    /// </summary>
+    public SummaryAttribute(string text)
+    {
+        Text = text;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string Text { get; set; }
+}
+
+public interface ISummaryAttribute
+{
+    public string Text { get; set; }
+}
