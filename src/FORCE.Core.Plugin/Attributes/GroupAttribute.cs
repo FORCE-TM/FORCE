dc4e81c197399d6360a1a532d8fce65fc5706dc8
Laiteux <matt@laiteux.dev>
2022-03-28 18:56:32 +0200

    Add GroupAttribute

---
+++ src/FORCE.Core.Plugin/Attributes/GroupAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace FORCE.Core.Plugin;
+
+/// <summary>
+/// Indicates that a class is a command group.<br/>
+/// Command groups allow to add a prefix to all of the commands in a class.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class GroupAttribute : Attribute, IGroupAttribute
+{
+    /// <summary>
+    /// <inheritdoc cref="GroupAttribute"/>
+    /// </summary>
+    /// <param name="prefixes">Prefix of the command group. Aliases can be added.</param>
+    public GroupAttribute(params string[] prefixes)
+    {
+        Prefixes = prefixes;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string[] Prefixes { get; set; }
+}
+
+public interface IGroupAttribute
+{
+    /// <summary>
+    /// The prefix and aliases of the command group.
+    /// </summary>
+    public string[] Prefixes { get; set; }
+}
