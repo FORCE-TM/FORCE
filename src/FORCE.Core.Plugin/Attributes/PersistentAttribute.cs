55ffc1f7e979487a28d87bffd63c7ce473f37ebb
Laiteux <matt@laiteux.dev>
2022-03-28 19:00:07 +0200

    Add PersistentAttribute

---
+++ src/FORCE.Core.Plugin/Attributes/PersistentAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace FORCE.Core.Plugin;
+
+/// <summary>
+/// Indicates that the value of a field or property should be preserved after a plugin reloads.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class PersistentAttribute : Attribute, IPersistentAttribute
+{
+    /// <summary>
+    /// <inheritdoc cref="PersistentAttribute"/>
+    /// </summary>
+    public PersistentAttribute()
+    {
+        IsPersistent = true;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public bool IsPersistent { get; set; }
+}
+
+public interface IPersistentAttribute
+{
+    public bool IsPersistent { get; set; }
+}
