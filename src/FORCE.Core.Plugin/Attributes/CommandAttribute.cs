f801a0b15dc8e79ca2b00d5720f3cf52776856fc
Laiteux <matt@laiteux.dev>
2022-03-28 18:56:10 +0200

    Add CommandAttribute

---
+++ src/FORCE.Core.Plugin/Attributes/CommandAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace FORCE.Core.Plugin;
+
+/// <summary>
+/// Indicates that a method is a plugin command.<br/>
+/// This must be used for a command to be discoverable.<br/>
+/// Unless this method is belongs to a <see cref="PluginBase"/> class, its parent class must inherit from <see cref="CommandBase"/>.<br/>
+/// In order to add a group prefix to a command, its parent class must be decorated with <see cref="GroupAttribute"/>.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class CommandAttribute : Attribute, ICommandAttribute
+{
+    /// <summary>
+    /// <inheritdoc cref="CommandAttribute"/>
+    /// </summary>
+    /// <param name="names">Name of the command. Aliases can be added.</param>
+    public CommandAttribute(params string[] names)
+    {
+        Names = names;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string[] Names { get; set; }
+}
+
+public interface ICommandAttribute
+{
+    /// <summary>
+    /// The name and aliases of the command.
+    /// </summary>
+    public string[] Names { get; set; }
+}
